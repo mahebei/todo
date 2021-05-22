83b618616f4ccf0bfa6687b193c696fdb751c51e
mahebei <31981108+mahebei@users.noreply.github.com>
2021-05-22 16:43:38 +1200

    comments

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,34 +5,29 @@ import './index.css'
 
 export default class Header extends Component {
 
-	//对接收的props进行：类型、必要性的限制
+	//set limit of props
 	static propTypes = {
 		addTodo:PropTypes.func.isRequired
 	}
 
-	//键盘事件的回调
+	//callback of keyboard event
 	handleKeyUp = (event)=>{
-		//解构赋值获取keyCode,target
 		const {keyCode,target} = event
-		//判断是否是回车按键
+		//check if it is ENTER
 		if(keyCode !== 13) return
-		//添加的todo名字不能为空
 		if(target.value.trim() === ''){
-			alert('输入不能为空')
+			alert('Item cannot be empty')
 			return
 		}
-		//准备好一个todo对象
 		const todoObj = {id:nanoid(),name:target.value,done:false}
-		//将todoObj传递给App
 		this.props.addTodo(todoObj)
-		//清空输入
 		target.value = ''
 	}
 
 	render() {
 		return (
 			<div className="todo-header">
-				<input onKeyUp={this.handleKeyUp} type="text" placeholder="请输入你的任务名称，按回车键确认"/>
+				<input onKeyUp={this.handleKeyUp} type="text" placeholder="Please input content, then press ENTER to add"/>
 			</div>
 		)
 	}
